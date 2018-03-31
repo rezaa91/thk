75f0605bf859cfff6739a3124e515ca017f2d034
Rezaa <rezaa91@hotmail.co.uk>
2018-03-31 12:06:34 +0100

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    entry: "./src/js/app.js",
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js'
+    },
+    module:{
+        rules:[
+            {
+                test: /\.css$/, 
+                loader:"style-loader!css-loader"
+            },
+            
+            {
+                test: /\.js$/, 
+                exclude: /node_modules/,
+                loader:'babel-loader',
+                options:{
+                    presets: ['env']
+                }
+            }
+        ]
+    }
+}
