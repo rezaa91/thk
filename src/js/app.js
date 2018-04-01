07ab80a7123f324fb409006dfcf04fb2eef58158
Rezaa91 <rezaa91@hotmail.co.uk>
2018-04-01 18:02:45 +0100

    removed bugs from form validation

--- src/js/app.js
+++ src/js/app.js
@@ -5,59 +5,81 @@ import 'bootstrap';
 
 
 $(function(){
+    
+    
     //fade in header and nav bar on load
     $('#main_header').hide().fadeIn(2000);
     $('#dl').hide().delay(1000).fadeIn(2000);
     $('#sub_header').hide().delay(1000).fadeIn(2000);
     $('.navbar li:not("#dl_link")').hide().delay(1500);
+    
     $('.navbar li:not("#dl_link")').each(function(i){
         $(this).delay(i*500).fadeIn(500);
     })
     
+
     
-    //VALIDATE FORM
-    let form = document.forms[0]; //cache form
-    let elements = form.elements; //cache form elements
-    let formSubmitted = false; //flag var
-    $('#submit').prop('disabled', true);
-    
-    let all = false;
-    
-    //change class dependent on user form input
-    for(let i = 0; i<elements.length; i++){
-        elements[i].addEventListener('blur', function() {
-            if(!this.value){ //if no value in input box
-                this.classList.add('validation');
-                all = false; //set flag var to false
-            }else{ //if value exists
-                this.classList.remove('validation');
-                all = true; //set flag to true
-            }
-            
-            checkForm(); //run checkForm function
-        })
-    }
-    
-    function checkForm(){ //check to see whether all form inputs have been inputted into
-        if(all && !formSubmitted){ //if all form inputs have value and form previously not sent
-            $('#submit').prop('disabled',false); //enable submit btn
-        }else{
-            $('#submit').prop('disabled',true); //disable submit btn
+    //FORM VALIDATION - IIFE
+    (function(){
+        //cache DOM values
+        let form = document.forms[0]; //form
+        let elements = form.elements; //elements
+        let submit = document.getElementById('submit'); //submit btn
+        
+        let formSubmitted = false; //flag var
+        
+        //check inputs - add error validation class to inputs with no value when blurred
+        for(let i = 0; i<elements.length-1; i++){
+            elements[i].addEventListener('blur', function() {
+                if(!this.value){
+                    this.classList.add('validation');
+                    if(this.classList.contains('correct')){
+                        this.classList.remove('correct'); //remove correct class if value removed
+                    }
+                    
+                }else{
+                    this.classList.remove('validation');
+                    this.classList.add('correct'); //give class 'correct' to all inputs with a value
+                }
+            })
         }
-    }
-    
-    
-    if(!formSubmitted) {//if form previously not submitted
-        form.addEventListener('submit', function(e) {
-            formSubmitted = true; //set flag  var to true
-            $('#submit').prop('disabled',true); //disable btn to prevent user from sending continious form results
-            console.log('form submitted');
-            
-            if(formSubmitted){
-                e.preventDefault(); //prevent form firing
+        
+        //check whether all form inputs have a value
+        function check(){
+            let hasVal = false; //set flag var to false
+            if(document.getElementsByClassName('correct').length === 4){
+                hasVal = true;
             }
             
+            return hasVal;
+        }
+        
+        form.addEventListener('submit', function(e) {
+            e.preventDefault(); //prevent form firing
+            let haveVals = check(); //check all form inputs have a value
+            
+            if(!formSubmitted && haveVals){//if form not previously submitted and all form values have inputs
+                if(submit.classList.contains('btn-danger')){
+                    submit.classList.remove('btn-danger'); //remove class if previous form submissions had errors
+                }
+                
+                submit.classList.add('btn-success');
+                submit.value = "SENT"; //inform user form has been sent
+                formSubmitted = true; //set flag var to true
+                submit.disabled = true;
+                
+                //make form inputs readonly
+                for(let i = 0; i<elements.length-1; i++){
+                    elements[i].setAttribute('readonly',true);
+                }
+                
+            }else{
+                submit.classList.add('btn-danger');
+                submit.value = "ERROR!";
+            }
         })
-    }    
+        
+    }());
+    
 })
 
